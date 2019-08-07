201aca467717ab9ef74371d3afb148a56239eb68
pranav1698 <hulkcodexl16@gmail.com>
2019-08-07 19:31:10 +0530

    Adding inerstion_sort algo

--- Sorting Algorithms/insertion_sort.cpp
+++ Sorting Algorithms/insertion_sort.cpp
@@ -15,11 +15,15 @@ int main(){
 	}
 
 	for(int i=0; i<n; i++){
-		for(int j=0; j<i; j++){
-			if(arr[j] > arr[j+1]){
-				swap(&arr[j], &arr[j+1]);
-			}
-		}
+		int key = arr[i];
+		int j = i - 1;  
+
+        while (j >= 0 && arr[j] > key) 
+        {  
+            arr[j + 1] = arr[j];  
+            j = j - 1;  
+        }  
+        arr[j + 1] = key;
 	}
 
 	for(int i=0; i<n; i++){
