ad30495c06702f369ff071591a0236325f278776
pranav1698 <hulkcodexl16@gmail.com>
2019-09-08 02:15:38 +0530

    Adding some modification

--- Sorting Algorithms/insertion_sort.cpp
+++ Sorting Algorithms/insertion_sort.cpp
@@ -16,13 +16,13 @@ int main(){
 
 	for(int i=0; i<n; i++){
 		int key = arr[i];
-		int j = i - 1;  
+		int j = i - 1;
 
-        while (j >= 0 && arr[j] > key) 
-        {  
-            arr[j + 1] = arr[j];  
-            j = j - 1;  
-        }  
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
         arr[j + 1] = key;
 	}
 
